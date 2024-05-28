c33b3eaa690347a99dffbf8696e914d2135605cd
bhoon-coding <bhooncoding@gmail.com>
2024-05-28 19:17:12 +0900

    Initial Commit

---
+++ Ijjeuminga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ijjeuminga
+//
+//  Created by BH on 2024/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
