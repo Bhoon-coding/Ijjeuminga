3d618fb958cc035eadf1538cec935d1e708adf8e
bhoon-coding <bhooncoding@gmail.com>
2024-06-23 11:10:11 +0900

    ignore test

--- Ijjeuminga/Util/Constants/Constant.swift
+++ Ijjeuminga/Util/Constants/Constant.swift
@@ -8,6 +8,5 @@
 import Foundation
 
 struct Constant {
-    // 테스트
     static let serviceKey: String = "24%2FpJnYDR4XV94MesDsNCLczRWS6TzkgXkazU4usR4gOt1mDnMi9i2hwfhMIQOa9rKP791WsLYupJwYxM%2FjXhQ%3D%3D"
 }
