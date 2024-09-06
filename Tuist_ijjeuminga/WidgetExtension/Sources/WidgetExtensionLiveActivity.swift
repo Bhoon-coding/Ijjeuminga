2632a0e8b086ce65321c68db065022fe688c70cc
bhoon-coding <bhooncoding@gmail.com>
2024-09-06 13:41:10 +0900

    chore: Activity View (Widget) 분리

--- Tuist_ijjeuminga/WidgetExtension/Sources/WidgetExtensionLiveActivity.swift
+++ Tuist_ijjeuminga/WidgetExtension/Sources/WidgetExtensionLiveActivity.swift
@@ -8,66 +8,89 @@ import Common
 import SwiftUI
 import WidgetKit
 
+struct ActivityView: View {
+    let context: ActivityViewContext<WidgetExtensionAttributes>
+    
+    var body: some View {
+        let attribute = context.attributes
+        let state = context.state
+        
+        let totalStop = 100.0
+        let currentValue = 35.0
+        
+        VStack(spacing: 24) {
+            // 상단 (로고 | 버스번호)
+            HStack {
+                Image(WidgetExtensionAsset.Images.splash.name)
+                    .resizable()
+                    .frame(width: 10, height: 40)
+                    .rotationEffect(Angle(degrees: -90))
+                    .padding(.leading, 16)
+                Spacer()
+                Text(state.busNumber)
+                    .foregroundStyle(.red)
+                    .fontWeight(.bold)
+            }
+            
+            // MARK: - 중간 (이번 정류장 | 정류장 이름 | 목적지 ~ 남음)
+            HStack {
+                Text(attribute.currentBusStopInfo)
+                    .font(.regular(14))
+                
+                Spacer()
+                
+                Text(state.currentBusStop)
+                    .font(.bold(24))
+                    .foregroundStyle(.green)
+                    .lineLimit(2)
+                
+                Spacer()
+                
+                Text("\(state.stopLeft) 정거장 남음")
+                    .font(.regular(14))
+                    
+            }
+            .foregroundStyle(WidgetExtensionAsset.Colors.subtitleText.swiftUIColor)
+            
+            // MARK: - ProgressBar
+            ZStack {
+                ProgressView(value: currentValue, total: totalStop)
+                    .tint(.green)
+                
+                GeometryReader { geometry in
+                    Image(WidgetExtensionAsset.Images.greenIcon.name)
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                        .position(
+                            x: geometry.size.width * CGFloat(currentValue / totalStop),
+                            y: geometry.size.height / 2
+                        )
+                    Image(WidgetExtensionAsset.Images.arriveIcon.name)
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                        .position(
+                            x: geometry.size.width - 8,
+                            y: geometry.size.height / 2
+                        )
+                }
+            }
+            
+        }
+        .foregroundStyle(.black)
+        .padding()
+        .activityBackgroundTint(.init(red: 239, green: 240, blue: 243))
+        .activitySystemActionForegroundColor(Color.black)
+
+    }
+}
+
 struct WidgetExtensionLiveActivity: Widget {
     var body: some WidgetConfiguration {
         ActivityConfiguration(for: WidgetExtensionAttributes.self) { context in
+            // MARK: - Widget
+            ActivityView(context: context)
             
-            let attribute: WidgetExtensionAttributes = context.attributes
-            let state: WidgetExtensionAttributes.ContentState = context.state
-            
-            let totalStop = 100.0
-            let currentValue = 35.0
-            
-            VStack(spacing: 24) {
-                // 상단 (로고 | 버스번호)
-                HStack {
-                    Text("이쯤인가(로고)")
-                    Spacer()
-                    Text(attribute.currentBusStopInfo)
-                        .foregroundStyle(.red)
-                        .fontWeight(.bold)
-                }
-                
-                // MARK: - 중간 (이번 정류장 | 정류장 이름 | 목적지 ~ 남음)
-                HStack {
-                    Text(attribute.currentBusStopInfo)
-                    Text(state.currentBusStop)
-                        .font(.title2)
-                        .foregroundStyle(.green)
-                    Text("목적지까지 \(state.stopLeft)정거장 남음")
-                }
-                
-                // MARK: - ProgressBar
-                ZStack {
-                    ProgressView(value: currentValue, total: totalStop)
-                        .tint(.green)
-                    
-                    GeometryReader { geometry in
-                        Image(systemName: "square.and.arrow.up.circle.fill")
-                            .resizable()
-                            .frame(width: 32, height: 32)
-                            .foregroundStyle(.green)
-                            .position(
-                                x: geometry.size.width * CGFloat(currentValue / totalStop),
-                                y: geometry.size.height / 2
-                            )
-                        Image(systemName: "paperplane.circle.fill")
-                            .resizable()
-                            .frame(width: 32, height: 32)
-                            .foregroundStyle(.blue)
-                            .position(
-                                x: geometry.size.width - 8,
-                                y: geometry.size.height / 2
-                            )
-                    }
-                }
-                
-            }
-            .foregroundStyle(.black)
-            .padding()
-            .activityBackgroundTint(.init(red: 239, green: 240, blue: 243))
-            .activitySystemActionForegroundColor(Color.black)
-
+            // MARK: - DynamicIsland
         } dynamicIsland: { context in
             DynamicIsland {
                 // Expanded UI goes here.  Compose the expanded UI through
@@ -118,7 +141,7 @@ extension WidgetExtensionAttributes {
 },
          contentStates: {
     WidgetExtensionAttributes.ContentState(
-        busNumber: 6002,
+        busNumber: "6002",
         currentBusStop: "동탄호수공원",
         stopLeft: 2,
         totalStop: 10
